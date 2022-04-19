96bbcb0292072f45ea5a2b441e0755a0efe7f7ed
Shrijan <maramshrijan001@gmail.com>
2022-04-19 14:46:54 +0800

    Added comments. Added empty tkinter window

--- main-program/what-CLI-main.py
+++ main-program/what-CLI-main.py
@@ -1,3 +1,5 @@
+from re import T
+from tkinter import ttk
 from tokenize import group
 import pywhatkit as what
 import time
@@ -51,7 +53,24 @@ def sendGroupMsg(lst_given, content, waiting_time=15):
             print("Error on group id {}\nError: {}".format(groupID, f"If we wait for {waiting_time} seconds on this group, it won't send the phone number on time. Try again."))
             print(dictTime)
 
-raw_df = pd.read_excel('./recepient_db.xlsx', converters={"phone": str})
+"""
+We will get the name, email, phone, package (free or paid), group_id (whatsapp group id) and greeting seperately from the excel file
+
+name: is used to greet the person with the name
+email: is not being used currently
+phone: is used to send a private message if there is no group
+package: free or paid, not being used currently
+group_id: is used to send messages to a group, is prioritised if both phone and group id are present
+greeting: is used for the greeting in groups, if absent when sending a group message, "name" will be used
+"""
+
+root = Tk()
+width, height = "1000", "600"
+root.geometry("{}x{}".format(width, height))
+root.title("WhatsApp Automation - AI Tech Park Sdn. Bhd.")
+# root.resizable(0,0)
+
+raw_df = pd.read_excel('./main-program/recepient_db.xlsx', converters={"phone": str})
 recepients = raw_df.fillna("null")
 
 lstRecepients = []
@@ -65,19 +84,24 @@ tempDictGroupIDs = {}
 for recepient in lstRecepients:
     name, email, phone, package, group_id, greeting = recepient[0], recepient[1], recepient[2], recepient[3], recepient[4], recepient[5]
     
+    # If there is phone and no group_id.
     if phone != "null" and group_id == "null":
         lstPhoneGroup.append((name, phone))
+    # If there is no phone and there is a group_id.
     elif phone == "null" and group_id != "null":
+        # If the group_id does not exist.
         if not group_id in tempDictGroupIDs.keys():
             tempDictGroupIDs[group_id] = name
             if greeting != "null":
                 tempDictGroupIDs[group_id] = greeting
         else:
+            # If there is no greeting but the group_id is present, raise error
             if greeting == "null":
                 print(f'Error on person {name}.\nError: Greeting must be present if sending through group for multiple people.')
                 break
             tempDictGroupIDs[group_id] = greeting
     
+    # If there is both phone and group_id
     elif phone != "null" and group_id != "null":
         if not group_id in tempDictGroupIDs.keys():
             tempDictGroupIDs[group_id] = name
@@ -89,6 +113,7 @@ for recepient in lstRecepients:
                 break
             tempDictGroupIDs[group_id] = greeting
     
+    # If both are not present
     elif phone == "null" and group_id == "null":
         print(f"The phone number or WhatsApp group id of {name} must be present.")
 
@@ -97,11 +122,12 @@ for id, greeting in tempDictGroupIDs.items():
 
 print(lstPhoneGroup, lstGroupSendGroup, tempDictGroupIDs, sep='\n')
 
-msg_f = "message.txt"
+msg_f = "./main-program/message.txt"
 
 with open(msg_f, 'r') as fh:
     msg = "".join(fh.readlines()[1:])
 
+# Getting the waiting seconds
 with open(msg_f) as fh:
     waiting_seconds_raw = fh.readlines()[0]
     waiting_seconds_index = waiting_seconds_raw.find(":")
@@ -114,11 +140,13 @@ with open(msg_f) as fh:
 #         lstPhNum.append(line)
 #################################
 
-sendMsg(lstPhoneGroup, msg, waiting_seconds)
-sendGroupMsg(lstGroupSendGroup, msg, waiting_time=waiting_seconds)
+# sendMsg(lstPhoneGroup, msg, waiting_seconds)
+# sendGroupMsg(lstGroupSendGroup, msg, waiting_time=waiting_seconds)
 
 ######## Testing Code ########
 # sendGroupMsg(lstGroupID, msg)
 # sendMsg(lstPhNum, msg, waiting_seconds)
 # print(msg)
 #################################
+
+root.mainloop()
